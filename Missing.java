5984004b65dc31aecb0643ff609bb419f15a9a3c
petrolhead-pj <paarthjain3010@gmail.com>
2025-11-16 14:46:19 +0530

    Create Missing.java

---
+++ Missing.java
@@ -0,0 +1,19 @@
+public class Missing{
+  public static void main(String args[]){
+
+  Scaner sc = new Scanner(System.in);
+  int arr[] = sc.nextInt();
+
+  for(int i=0; i<arr.length; i++)
+    {
+      int sum;
+      sum = sum+a[i]
+        }
+  int n = arr.length 
+  int total = (n * (n+1))/2;
+
+int missing = total - sum;
+
+return missing
+  }
+}
